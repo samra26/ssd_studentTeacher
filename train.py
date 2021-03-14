5ff55108236fd97f9acb8a93ca312cb7fe40b57e
samra26 <75056386+samra26@users.noreply.github.com>
2021-03-15 00:15:45 +0500

    Update train.py

--- train.py
+++ train.py
@@ -164,8 +164,12 @@ def train():
             adjust_learning_rate(optimizer, args.gamma, step_index)
 
         # load train data
-        images, targets = next(batch_iterator)
-
+        #images, targets = next(batch_iterator)
+        try:
+            images, targets = next(batch_iterator)
+        except StopIteration:
+            batch_iterator=iter(data_loader)
+            images, targets = next(batch_iterator)
         if args.cuda:
             images = Variable(images.cuda())
             targets = [Variable(ann.cuda(), volatile=True) for ann in targets]
